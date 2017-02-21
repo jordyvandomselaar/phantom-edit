6557a11c80097452ccb6ae48147e8d06d19d91b0
concept-core <wouter.van.marrum@concept-core.nl>
2017-02-21 19:48:20 +0000

    Updated the main controller so it will be able to fetch data for the website itself

--- app/Http/Controllers/Website/MainController.php
+++ app/Http/Controllers/Website/MainController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers\Website;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Website\Website;
+use Request;
+use View;
 
+/**
+ * Class    MainController
+ * @package    App\Http\Controllers\Website
+ */
 class MainController extends Controller
 {
-    public function getPage($page)
-    {
-    }
+	/**
+	 * Gets the page content.
+	 *
+	 * @param      string  $route  The route
+	 *
+	 * @return     404 | View  The page content.
+	 */
+	public function getPageContent($route = "/")
+	{
+		$content = Website::with(['pages' => function($query) use ($route) {
+			return $query->with('content')->where('slug', '=', $route);
+		}])->where('name', '=', 'phantom-edit.app')->first();
+
+		if (is_null($content)) {
+			abort(404);
+		}
+
+		return View::make($this->theme_folder.$content->pages->first()->view_file)
+			->withPageContent($content->pages->first());
+	}
 }
