e0829d399603c6df9c77298f283526fe6fe5e7cd
concept-core <wouter.van.marrum@concept-core.nl>
2017-02-21 19:47:44 +0000

    Updated the base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,20 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Get the config value from the phantom config
+     *
+     * @param      string  $key    The key
+     *
+     * @return     string  The value of the key
+     */
+    public function __get($key)
+    {
+    	$searchKey = str_replace("_", ".", $key);
+
+    	if (!is_null(config("phantom.{$searchKey}"))) {
+    		return config("phantom.{$searchKey}");
+    	}
+    }
 }
