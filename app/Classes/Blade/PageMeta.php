108ff261a9281c0acc671883037c9767d1b61703
Wouter van Marrum <wouter.van.marrum@concept-core.nl>
2017-03-02 00:10:00 +0100

    Updated complete page meta helper

--- app/Classes/Blade/PageMeta.php
+++ app/Classes/Blade/PageMeta.php
@@ -3,138 +3,140 @@
 namespace App\Classes\Blade;
 
 /**
- * Class for page meta.
+ * Class PageMeta
+ * @package App\Classes\Blade
  *
- * @author     Wouter van Marrum <w.vanmarrum@texemus.com>
- *
- * @package    App\Classes\Blade
+ * @author Wouter van Marrum <w.vanmarrum@texemus.com>
  */
 class PageMeta
 {
-	protected $pageContent;
+    protected $html;
 
-	protected $robots = [
+    /**
+     * @var
+     */
+    protected $pageContent;
+
+    /**
+     * @var array
+     */
+    protected $robots = [
 		'index, follow',
 		'noindex, follow',
 		'index, nofollow',
 		'noindex, nofollow'
 	];
 
-	protected $revisit = [
+    /**
+     * @var array
+     */
+    protected $revisit = [
 		'days',
 		'month'
 	];
 
-	/**
-	 * Gets the information.
-	 *
-	 * @param      Object  $pageContent  The page content
-	 * @param      array   $attr         The attribute
-	 *
-	 * @return     <type>  The information.
-	 */
+    /**
+     * Gets the page information
+     *
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     * @param $pageContent
+     * @param array $attr
+     * @return mixed
+     */
 	public function getInformation($pageContent, $attr = [])
 	{
 		$this->pageContent = $pageContent;
 		return $this->buildMetaTags($attr);
 	}
 
-	/**
-	 * Builds meta tags.
-	 *
-	 * @return     <type>  The meta tags.
-	 */
+    /**
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     * @param $attr
+     * @return mixed
+     */
 	public function buildMetaTags($attr)
 	{
 		$this->buildTitleTag()
 			->buildDescriptionTag()
 			->buildKeywordsTag()
-			->buildAuthorTag($attr['author'])
-			->buildRobotsTag($attr['robots'])
+			->buildAuthorTag($attr['author'] ?? null)
+			->buildRobotsTag($attr['robots'] ?? [])
 			->buildRevisitAfterTag($attr['revisit'] ?? []);
+
+		return $this->html;
 	}
 
-	/**
-	 * Builds a title tag.
-	 *
-	 * @return     string  The title tag.
-	 */
+    /**
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     * @return $this
+     */
 	public function buildTitleTag()
 	{
-		echo "<title>". $this->pageContent->name ."</title>\n";
+		$this->html .= "<title>". $this->pageContent->name ."</title>\n";
 		return $this;
 	}
 
-	/**
-	 * Builds a description tag.
-	 *
-	 * @return     string  The description tag.
-	 */
+    /**
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     * @return $this
+     */
 	public function buildDescriptionTag()
 	{
 		if (!is_null($this->pageContent->description)) {
-			echo "<meta name=\"description\" content=\"". $this->pageContent->description ."\" />\n";
+			$this->html .= "<meta name=\"description\" content=\"". $this->pageContent->description ."\" />\n";
 		}
 		return $this;
 	}
 
-	/**
-	 * Builds a keywords tag.
-	 *
-	 * @return     string  The keywords tag.
-	 */
+    /**
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     * @return $this
+     */
 	public function buildKeywordsTag()
 	{
 		if (!is_null($this->pageContent->keywords)) {
-			echo "<meta name=\"keywords\" content=\"". $this->pageContent->keywords ."\" />\n";
+			$this->html .= "<meta name=\"keywords\" content=\"". $this->pageContent->keywords ."\" />\n";
 		}
 		return $this;
 	}
 
-	/**
-	 * Builds an author tag.
-	 *
-	 * @param      <type>  $author  The author
-	 *
-	 * @return     string  The author tag.
-	 */
+    /**
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     * @param $author
+     * @return $this
+     */
 	public function buildAuthorTag($author)
 	{
 		if (isset($author)) {
-			echo "<meta name=\"author\" content=\"$author\" />\n";
+			$this->html .= "<meta name=\"author\" content=\"$author\" />\n";
 		}
 
 		return $this;
 	}
 
-	/**
-	 * Builds a robots tag.
-	 *
-	 * @param      <type>  $type   The type
-	 *
-	 * @return     string  The robots tag.
-	 */
+    /**
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     * @param $attr
+     * @return $this
+     */
 	public function buildRobotsTag($attr)
 	{
 		if (isset($attr['type'])) {
-			echo "<meta name=\"robots\" content=\"". $this->robots[$attr['type']] ."\" />\n";
+			$this->html .= "<meta name=\"robots\" content=\"". $this->robots[$attr['type']] ."\" />\n";
 		}
 
 		return $this;
 	}
 
-	/**
-	 * Builds a revisit after tag.
-	 *
-	 * @param      <type>  $type    The type
-	 * @param      <type>  $amount  The amount
-	 *
-	 * @return     string  The revisit after tag.
-	 */
+    /**
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     * @param $attr
+     * @return $this
+     */
 	public function buildRevisitAfterTag($attr)
 	{
 		if (isset($attr['amount']) && isset($attr['type'])) {
-			echo "<meta name=\"revisit-after\" content=\"". $attr['amount'] . " " . $this->revisit[$attr['type']] ."\" />\n";
+			$this->html .= "<meta name=\"revisit-after\" content=\"". $attr['amount'] . " " . $this->revisit[$attr['type']] ."\" />\n";
 		}
 
 		return $this;
