6f1d574b846d4b7591eae853bfddc4bfd8c9d7db
Wouter van Marrum <w.vanmarrum@texemus.com>
2017-03-06 19:22:19 +0000

    Created entities model

--- app/Models/Core/Entities.php
+++ app/Models/Core/Entities.php
@@ -4,7 +4,35 @@ namespace App\Models\Core;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Entities
+ * @package App\Models\Core
+ * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+ */
 class Entities extends Model
 {
-    //
+    protected $table = "entities";
+
+    protected $fillable = [
+        'name',
+        'model',
+        'views',
+        'name_multiple',
+        'name_single',
+    ];
+
+    /**
+     * @param $entity
+     *
+     * @author Wouter van Marrum <w.vanmarrum@texemus.com>
+     *
+     * @return mixed
+     */
+    public static function getEntity($entity)
+    {
+        return Entities::where("name", "=", $entity)
+            ->orWhere('name_multiple', '=', $entity)
+            ->orWhere('name_single', '=', $entity)
+            ->firstOrFail();
+    }
 }
